f534cab2f2a0ed6fe9fa987ee8675d5bd284e4c3
Remo <git@rwalser.ch>
2019-02-07 22:47:19 +0100

    Login Logout

--- car/views.py
+++ car/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/car/login/')
 def index(request):
 
     template = loader.get_template('car/index.html')
