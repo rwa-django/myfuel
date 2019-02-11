a6c04a6a5e76ef95bf4e42a6165503cba057daea
Remo <git@rwalser.ch>
2019-02-11 23:10:41 +0100

    DB change Car

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vehicle_Fuel, Vehicle_Fuel_Pos, Vehicle_Type
+
+
+class VehicleTypeAdmin(admin.ModelAdmin):
+    list_display = ('login', 'type', 'aktiv', 'label')
+
+
+class VehicleFuelAdmin(admin.ModelAdmin):
+    list_display = ('login', 'type', 'booked',)
+    search_fields = ('login', 'type',)
+
+class VehicleFuelPosAdmin(admin.ModelAdmin):
+    list_display = ('car_id', 'pos', 'amount', 'km', 'liter', 'booked')
+
+admin.site.register(Vehicle_Type, VehicleTypeAdmin)
+admin.site.register(Vehicle_Fuel, VehicleFuelAdmin)
+admin.site.register(Vehicle_Fuel_Pos, VehicleFuelPosAdmin)
+
--- car/models.py
+++ car/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+# Vehicle Type
+class Vehicle_Type(models.Model):
+    login = models.CharField(max_length=200)
+    type = models.CharField(max_length=10,
+                            default='CAR',
+                            help_text="Type")
+    pos = models.SmallIntegerField(default=1)
+    aktiv = models.BooleanField(default=False,
+                                help_text="Aktiv")
+    label = models.CharField(max_length=200,
+                             help_text="Beschreibung")
+
+    def __str__(self):
+        return '{0} - {1}'.format(self.login, self.label)
+
+    class Meta:
+        unique_together = (('login', 'type', 'pos'),)
+        ordering = ['login', 'pos']           # sortierung mit - dreht die Sortierung
+
+# Vehicle Fuel Header
+class Vehicle_Fuel(models.Model):
+    login = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE,
+                              editable=True)
+    type = models.ForeignKey(Vehicle_Type,
+                             default=1,
+                             on_delete=models.CASCADE)
+    booked = models.DateTimeField(default=timezone.now,
+                                  editable=False)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.login, self.type)
+
+    class Meta:
+        unique_together = (('login', 'type'),)
+
+
+# Vehicle Fuel Positions
+class Vehicle_Fuel_Pos(models.Model):
+    car_id = models.ForeignKey(Vehicle_Fuel,
+                               on_delete=models.CASCADE)
+    pos = models.SmallIntegerField(default=1)
+    booked = models.DateTimeField(default=timezone.now,
+                                  editable=False)
+    amount = models.DecimalField(max_digits=10,
+                                 decimal_places=2,
+                                 help_text="Kosten")
+    km = models.PositiveSmallIntegerField(default=0,
+                                              help_text="KM Stand")
+    liter = models.DecimalField(default=0,
+                                max_digits=10,
+                                decimal_places=2,
+                                help_text="Liter")
+    info = models.CharField(max_length=200,
+                            help_text="Buchungs Info")
+
+    def __str__(self):
+        return '{0} - {1} / {2}/{3}/{4}'.format(self.pos, self.amount, self.km, self.liter, self.booked)
+
+    class Meta:
+        unique_together = (('car_id', 'pos'),)
+        ordering = ['car_id', 'pos']           # sortierung mit - dreht die Sortierung
